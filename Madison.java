8e12622941e3eed9ec52e0ad1f8a109b0bb8d608
madison-nitti <nmadison06@vt.edu>
2026-09-24 01:41:25 -0400

    update feedbackgenerator.java

--- Madison.java
+++ Madison.java
@@ -1,6 +1,48 @@
-public class Madison
+public class FeedbackGenerator
 {
-    public static void main(String[] args) {
-        System.out.println("Madison is on the team.");
+
+    /**
+     * Creates new Feedback Generator.
+     */
+    public FeedbackGenerator()
+    {
+        // no fields need to be initialized
+    }
+
+
+    /**
+     * Compares a guess to a target word, and will return feedback for each
+     * letter.
+     * 
+     * @param guess
+     *            the player's valid guess
+     * @param target
+     *            the target word
+     * @return the feedback for the guess
+     */
+
+    public String generateFeedBack(String guess, String target)
+    {
+        String feedback = "";
+
+        for (int i = 0; i < guess.length(); i++)
+        {
+            char guessLetter = guess.charAt(i);
+
+            if (guessLetter == target.charAt(i))
+            {
+                feedback = feedback + "✔";
+
+            }
+            else if (target.indexOf(guessLetter) != -1)
+            {
+                feedback = feedback + "~";
+            }
+            else
+            {
+                feedback = feedback + "X";
+            }
+        }
+        return feedback;
     }
 }
