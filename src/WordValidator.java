3bd2c6199cbd23e30936af13a2b469e021dca2e9
ckavuri2026S <ckavuri@vt.edu>
2026-09-24 08:55:15 -0400

    Update WordValidator.java

--- src/WordValidator.java
+++ src/WordValidator.java
@@ -1,8 +1,76 @@
+import java.util.ArrayList;
+//-------------------------------------------------------------------------
+/**
+* Is the guess a valid guess
+* 
+* @author chandrikakavuri
+* @version Sep 22, 2026
+*/
+
 public class WordValidator {
-    //~ Fields ................................................................
 
-    //~ Constructors ..........................................................
+    private WordBank wordBank;
 
-    //~Public  Methods ........................................................
+    // ----------------------------------------------------------
+    /**
+     * Create a new WordValidator object.
+     * @param wordBank
+     */
+    public WordValidator(WordBank wordBank) {
+        this.wordBank = wordBank;
+    }
 
+    /**
+     * Checks whether a word is valid.
+     *
+     * @param previousGuesses array list of previously guessed words
+     * @param guess entered guess
+     * @return whether the word is valid
+     */
+    public boolean isValidGuess(String guess, ArrayList<String> previousGuesses) {
+
+        if (guess == null) {
+            return false;
+        }
+
+        guess = guess.toLowerCase();
+
+        if (guess.length() != 5) {
+            return false;
+        }
+
+        for (int i = 0; i < guess.length(); i++) {
+            if (!Character.isLetter(guess.charAt(i))) {
+                return false;
+            }
+        }
+
+        if (!wordBank.containsWord(guess)) {
+            return false;
+        }
+
+        if (isRepeatedGuess(guess, previousGuesses)) {
+            return false;
+        }
+
+        return true;
+    }
+    
+    /**
+     * Checks whether a word is valid.
+     *
+     * @param previousGuesses array list of previously guessed words
+     * @param guess entered guess
+     * @return whether the current guess has already been guessed in the current game
+     */
+    public boolean isRepeatedGuess(String guess, ArrayList<String> previousGuesses) {
+
+        if (guess == null) {
+            return false;
+        }
+
+        guess = guess.toLowerCase();
+
+        return previousGuesses.contains(guess);
+    }
 }
